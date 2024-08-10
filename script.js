b284b33168ab889d839c52e673751c93cdd08011
Debargha Mondal <debarghamondal227@gmail.com>
2024-08-10 17:34:06 +0530

    weather-app

---
+++ script.js
@@ -0,0 +1,109 @@
+const apiKey = "aef74094556e5a489024a5faab22cf6a";
+
+
+const weatherDataEle = document.querySelector(".weather-data");
+const cityNameEle = document.querySelector("#city-name");
+const formEle = document.querySelector("form");
+const imgIcon = document.querySelector(".icon");
+
+
+formEle.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const cityValue = cityNameEle.value;
+
+    getWeatherData(cityValue)
+})
+
+async function getWeatherData(cityValue) {
+
+    try {
+        const response = await
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`)
+
+        if (!response.ok) {
+            throw new Error("Network response is not ok!")
+        }
+
+        const data = await response.json()
+
+        const temprature = Math.floor(data.main.temp);
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+
+
+        const details = [`Feels Like: ${Math.floor(data.main.feels_like)}°C`, `Humidity: ${data.main.humidity}%`, `Wind Speed: ${data.wind.speed} m/s`]
+
+
+        weatherDataEle.querySelector(".temp").textContent = `${temprature}°C`
+        weatherDataEle.querySelector(".desc").textContent = `${description}`
+
+        imgIcon.innerHTML = `<img src="https://openweathermap.org/img/w/${icon}.png" alt="">`
+
+
+        weatherDataEle.querySelector(".details").innerHTML = details.map((detail) => {
+            return `<div>${detail}</div>`
+        }).join("")
+
+
+    } catch (err) {
+        weatherDataEle.querySelector(".temp").textContent = ""
+        imgIcon.innerHTML = ""
+        weatherDataEle.querySelector(".desc").textContent = "An Error Occurred!"
+    }
+
+}
+
+function getCurrentLocationWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            getWeatherDataByCoords(lat, lon)
+        },
+            () => {
+                weatherDataEle.querySelector(".temp").textContent = "";
+                imgIcon.innerHTML = "";
+                weatherDataEle.querySelector(".desc").textContent = "Unable to retrieve your location";
+            }
+        );
+
+    } else {
+        weatherDataEle.querySelector(".temp").textContent = "";
+        imgIcon.innerHTML = "";
+        weatherDataEle.querySelector(".desc").textContent = "Geolocation is not supported by this browser."
+    }
+}
+
+
+async function getWeatherDataByCoords(lat, lon) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+        if (!response.ok) {
+            throw new Error("Network response is not ok!");
+        }
+
+        const data = await response.json();
+
+        const temperature = Math.floor(data.main.temp);
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+
+        const details = [
+            `Feels Like: ${Math.floor(data.main.feels_like)}°C`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind Speed: ${data.wind.speed} m/s`
+        ];
+        weatherDataEle.querySelector(".temp").textContent = `${temperature}°C`;
+        weatherDataEle.querySelector(".desc").textContent = `${description}`;
+        imgIcon.innerHTML = `<img src="https://openweathermap.org/img/w/${icon}.png" alt="">`;
+
+        weatherDataEle.querySelector(".details").innerHTML = details.map(detail => `<div>${detail}</div>`).join("");
+
+    } catch (err) {
+        weatherDataEle.querySelector(".temp").textContent = "";
+        imgIcon.innerHTML = "";
+        weatherDataEle.querySelector(".desc").textContent = "An Error Occurred!";
+    }
+}
+
+getCurrentLocationWeather();
